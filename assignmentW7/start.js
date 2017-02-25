bef93ff7f3b39bf58288f61c65c0480c1b52afec
BeNsAeI <behnam.saeidi@gmail.com>
2017-02-24 23:12:22 -0800

    log working

---
+++ assignmentW7/start.js
@@ -0,0 +1,75 @@
+function Server(req,res){
+	res.render("server",curTime);
+}
+function reqGetHandler(req,res){
+	var inBound = {};
+	inBound.data = [];
+	inBound.type = "Get";
+	var time = (new Date(Date.now())).toLocaleTimeString('en-US');
+	for(var query in req.query){
+		inBound.data.push({'name':query,'value':req.query[query],'time':"TIME"});
+		log.push({'method':"Get",'name':query,'value':req.query[query]});
+	}
+	inBound.log = log;
+	res.render('post',inBound);
+}
+function reqPostHandler(req,res){
+	var inBound = {};
+	inBound.data = [];
+	inBound.type = "Post";
+	var time = (new Date(Date.now())).toLocaleTimeString('en-US');
+	for(query in req.body){
+		inBound.data.push({'name':query,'value':req.body[query],'time':"TIME"});
+		log.push({'method':"Post",'name':query,'value':req.body[query]});
+	}
+	inBound.log = log;
+	res.render('post',inBound);
+	console.log(inBound);
+	console.log(req.body);
+}
+function handler404(req,res){
+	res.type('text/plain');
+	res.status(404);
+	res.send('404 - Not Found');
+}
+function handler500(err,req,res,next){
+	console.error(err.stack);
+	res.type('plain/text');
+	res.status(500);
+	res.send('500 - Server Error');
+}
+function logOnSuccess(){
+	console.log("Express started on http:\/\/localhost:" + app.get('port') + '; press Ctrl-C to terminate.');
+}
+
+var curTime={};
+curTime.serverStartTime = (new Date(Date.now())).toLocaleTimeString('en-US');
+
+var express = require('express');
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+var app = express();
+
+// For logging purposes
+var log = [];
+
+// for form handling:
+app.use(bodyParser.urlencoded({extended:false}));
+// for json handling:
+app.use(bodyParser.json());
+
+app.engine('handlebars',handlebars.engine);
+app.set('view engine','handlebars');
+app.set('port', 1852);
+
+app.get('/server',Server);
+
+app.get('/',reqGetHandler);
+
+app.post('/',reqPostHandler);
+
+app.use(handler404);
+
+app.use(handler500);
+
+app.listen(app.get('port'), logOnSuccess);
